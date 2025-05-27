b8efc3708819c33e1e8a1c339a98f732c1986cfa
Snej69420 <Jens.Vissenberg@student.uantwerpen.be>
2025-05-27 09:20:46 +0200

    Forgot to include the loyalty_card.py model

---
+++ addons/sale/models/loyalty_card.py
@@ -0,0 +1,60 @@
+import ast
+import json
+
+from pygments.lexer import default
+
+from odoo import _, api, fields, models
+
+class LoyaltyCard(models.Model):
+    _name = "sale.loyalty.card"
+    _description = "Loyalty Card"
+
+    _sql_constraints = [
+        ('check_loyalty_points',
+         'CHECK(points >= 0)',
+         'The number of loyalty points can\'t be negative.'
+        ),
+        ('check_conversion_rate',
+         'CHECK(conversion_rate > 0)',
+         'The conversion rate must be greater than 0.'
+        ),
+        ('check_threshold',
+         'CHECK(threshold > 0)',
+         'The threshold must be greater than 0.'
+        ),
+        ('check_currency_discount',
+         'CHECK(currency_discount > 0)',
+         'The currency discount must be greater than 0.'
+        ),
+        ('check_percentage_discount',
+         'CHECK(percentage_discount > 0 AND percentage_discount <= 1)',
+         'The percentage discount must be greater than 0%.'
+        ),
+        ('check_max_discount_amount',
+         'CHECK(max_discount_amount >= 0)',
+         'The max_discount_amount can\'t be negative.'
+        ),
+        (
+            'unique_customer_company_card',
+            'unique(partner_id, company_id)',
+            'A customer can have only one loyalty card per company.'
+        ),
+    ]
+
+    name = fields.Char(string="Loyalty Card Name", default="Global", required=True)
+    points = fields.Float(string="Loyalty Points", digits=(16, 2), default=0)
+    conversion_rate = fields.Float(string="Conversion Rate", help="Points earned per unit of currency spent", digits=(16, 2), default=0.2)
+    threshold = fields.Integer(string="Point Threshold", help="Points required to earn a discount.", default=100)
+
+    currency_id = fields.Many2one('res.currency', string='Currency', required=True, default=lambda self: self.env.company.currency_id)
+    currency_discount = fields.Monetary(string="Discount", currency_field='currency_id', default=5)
+    percentage_discount = fields.Float(string="Discount Percentage", help="Represented as all values between zero and one.", digits=(16, 2), default=0.05)
+    discount_type = fields.Selection(string="Discount Type", help="Wether the discount will be in percentages or currency.",
+                                     selection=[("c", "currency"), ("p", "percentage")], default="p") # not fully defined yet
+
+    max_discount = fields.Boolean(string="Limit Discount", default=False)
+    max_discount_amount = fields.Integer(string="Maximum Discount Allowed", default=50)
+
+    # links loyalty card, with customer and company pair(s).
+    partner_id = fields.Many2one('res.partner', string="Partner", required=True, domain="[('customer_rank', '>', 0)]",)
+    company_id = fields.Many2one('res.company', string="Company", required=True)
